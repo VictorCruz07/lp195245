24f49186b71493286b5df62e7f111d2a293fcef8
VictorCruz07 <victor.cruz22@unifesp.br>
2026-04-11 20:55:55 -0300

    Create 2758.C

---
+++ 2758.C
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main()
+{
+	float ps1 , ps2;
+    double pd1 , pd2;
+	scanf("%f %f %lf %lf",&ps1,&ps2,&pd1,&pd2);
+	printf("A = %f, B = %f\n", ps1, ps2);
+	printf("C = %lf, D = %lf\n", pd1, pd2);
+    printf("A = %.1f, B = %.1f\n", ps1, ps2);
+	printf("C = %.1lf, D = %.1lf\n", pd1, pd2);
+	printf("A = %.2f, B = %.2f\n", ps1, ps2);
+	printf("C = %.2lf, D = %.2lf\n", pd1, pd2);
+	printf("A = %.3f, B = %.3f\n", ps1, ps2);
+	printf("C = %.3lf, D = %.3lf\n", pd1, pd2);
+	printf("A = %.3E, B = %.3E\n", ps1, ps2);
+	printf("C = %.3E, D = %.3E\n", pd1, pd2);
+	printf("A = %.0f, B = %.0f\n", ps1, ps2);
+	printf("C = %.0lf, D = %.0lf\n", pd1, pd2);
+	
+return 0;
+
+}
