0d1a4d494b9ccdbbb14d6744dee87c8af053cf2c
VictorCruz07 <victor.cruz22@unifesp.br>
2026-04-20 19:06:22 -0300

    Update 1000.c

--- 1000.c
+++ 1000.c
@@ -1,3 +1,13 @@
+/* --------------------------------------------------------------------------
+Disciplina  : Lógica de Programação, turma IB, 2026S1
+Linguagem   : C
+Nome        : Victor Cruz de Farias
+Plataforma  : https://judge.beecrowd.com/pt/search
+Exercício   : 1000
+Data        : 14/04/2026
+Objetivo    : Escrever Hello World
+Aprendizado : inicio da linguagem C
+-------------------------------------------------------------------------- */
 #include <stdio.h>
 
 int main()
