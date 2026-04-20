dae272f6ead8b1157e76bde476d3f69d6c13d562
VictorCruz07 <victor.cruz22@unifesp.br>
2026-04-20 19:13:39 -0300

    Update 1000.c

--- 1000.c
+++ 1000.c
@@ -1,5 +1,5 @@
 /* --------------------------------------------------------------------------
-Disciplina  : Lógica de Programação, turma IB, 2026S1
+Disciplina  : Lógica de Programação, turma IB, 195245
 Linguagem   : C
 Nome        : Victor Cruz de Farias
 Plataforma  : https://judge.beecrowd.com/pt/search
