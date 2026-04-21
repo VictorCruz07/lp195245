dd6ae29ee3e3f7f937501f8c23b873f72c19f55b
VictorCruz07 <victor.cruz22@unifesp.br>
2026-04-21 11:17:20 -0300

    Create 1080.C

---
+++ 1080.C
@@ -0,0 +1,11 @@
+/* --------------------------------------------------------------------------
+Disciplina  : Lógica de Programação, turma IB, 2026S1
+Linguagem   : C
+Nome        : Victor Cruz de Farias
+Plataforma  : https://judge.beecrowd.com/pt/search
+Exercício   : 1000
+Data        : 21/04/2026
+Objetivo    : Mostrar a soma entre 2 valores 
+Aprendizado : Usar repetições 
+-------------------------------------------------------------------------- */
+#include <stdio.h>
