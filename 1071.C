7ad23477ae5d4b3b920a04efd6ce975c534b4652
VictorCruz07 <victor.cruz22@unifesp.br>
2026-04-21 11:16:11 -0300

    Update 1071.C

--- 1071.C
+++ 1071.C
@@ -1,1 +1,29 @@
+/* --------------------------------------------------------------------------
+Disciplina  : Lógica de Programação, turma IB, 2026S1
+Linguagem   : C
+Nome        : Victor Cruz de Farias
+Plataforma  : https://judge.beecrowd.com/pt/search
+Exercício   : 1000
+Data        : 10/04/2026
+Objetivo    : Mostrar a soma entre 2 valores 
+Aprendizado : Usar repetições 
+-------------------------------------------------------------------------- */
+#include <stdio.h>
 
+int main() {
+  int X,Y,Maior,Menor,Soma=0 ;
+  scanf("%d %d" , &X , &Y);
+  if (X>Y) {
+    Maior=X;
+    Menor=Y;
+    printf ("Maior=%d \nMenor=%d", Maior, Menor);
+  }
+    else{
+        Maior=Y;
+        Menor=X;
+    printf ("Maior=%d \nMenor=%d\n", Maior, Menor);
+    }   
+    Soma =Maior+Menor;
+    printf ("Soma=%d", Soma);
+return 0;
+}
