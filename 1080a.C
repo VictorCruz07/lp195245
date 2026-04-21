5048c2c7bbdc462ce8cb5cfb759222c579500047
VictorCruz07 <victor.cruz22@unifesp.br>
2026-04-21 11:51:31 -0300

    Update and rename 1080.C to 1080a.C

--- 1080a.C
+++ 1080a.C
@@ -3,9 +3,23 @@ Disciplina  : Lógica de Programação, turma IB, 2026S1
 Linguagem   : C
 Nome        : Victor Cruz de Farias
 Plataforma  : https://judge.beecrowd.com/pt/search
-Exercício   : 1000
+Exercício   : 1080
 Data        : 21/04/2026
-Objetivo    : Mostrar a soma entre 2 valores 
-Aprendizado : Usar repetições 
+Objetivo    : Mostrar o Maior numero e a posição dele sem uso de array
+Aprendizado : usar o IF
 -------------------------------------------------------------------------- */
 #include <stdio.h>
+
+int main() {
+  int i,maior=0,posicao=0,valor;
+  for (i=1; i <= 100; i++){
+    scanf ("%d", &valor);
+    if (valor>maior) {
+    maior = valor;
+    posicao = i;
+    }
+  }
+  printf ("%d\n", maior);
+  printf ("%d\n", posicao);
+ return 0;
+}
