c7bdebefe554dccdbf6de2347a348596d71f66ee
VictorCruz07 <victor.cruz22@unifesp.br>
2026-04-28 17:01:48 -0300

    Create 1078.C

---
+++ 1078.C
@@ -0,0 +1,23 @@
+/* --------------------------------------------------------------------------
+Disciplina  : Lógica de Programação, turma IB, 2026S1
+Linguagem   : C
+Nome        : Victor Cruz de Farias
+Plataforma  : https://judge.beecrowd.com/pt/search
+Exercício   : 1078
+Data        : 28/04/2026
+Objetivo    : tabuada do numero escrito
+Aprendizado : Fazer uma tabuada
+-------------------------------------------------------------------------- */
+#include <stdio.h>
+
+int main()
+{
+    int A, B, C;
+    scanf ("%d", &A);
+    for (B=1; B<=10; B++){
+        C = B * A;
+    
+    printf ("%d x %d = %d\n", B , A , C);
+    }
+    return 0;
+}
