5521bd2a009901123b8753dcbd6fcd80b1f30f00
VictorCruz07 <victor.cruz22@unifesp.br>
2026-05-14 14:22:22 -0300

    Create 1015.C

---
+++ 1015.C
@@ -0,0 +1,27 @@
+/* --------------------------------------------------------------------------
+Disciplina  : Lógica de Programação, turma IB, 2026S1
+Linguagem   : C
+Nome        : Victor Cruz de Farias
+Plataforma  : https://judge.beecrowd.com/pt/search
+Exercício   : 1015
+Data        : 14/05/2026
+Objetivo    : Calcular a distancia entre dois ponts em um plano cartesiano XY
+Aprendizado : Usar raiz quadrada e elevar um valor ao quadrado com uma nova biblioteca 
+-------------------------------------------------------------------------- */
+#include <stdio.h>
+#include <math.h>
+
+int main()
+{
+    double x1, y1, x2, y2, distancia, base1, base2, A, B;
+    scanf ("%lf %lf %lf %lf", &x1 , &y1 , &x2 , &y2);
+    base1 = x2 - x1;
+    base2 = y2 - y1;
+    A = pow(base1 , 2.0);
+    B = pow(base2 , 2.0);
+    distancia = sqrt(A + B);
+    printf ("%.4lf\n", distancia);
+    
+    return 0;
+}
+    
