ad431f2236cf208aab029a3105ef66751d907cd5
VictorCruz07 <victor.cruz22@unifesp.br>
2026-06-20 11:25:30 -0300

    Update 1024.C

--- 1024.C
+++ 1024.C
@@ -2,8 +2,7 @@
 Disciplina  : Lógica de Programação, turma IB, 2026S1
 Linguagem   : C
 Nome        : Victor Cruz de Farias
-Plataforma  : https://judge.beecrowd.com/pt/search
-Exercício   : 1024
+Exercício   : https://judge.beecrowd.com/pt/problems/view/1024
 Data        : 07/05/2026
 Objetivo    : Criptografar 
 Aprendizado : Manipulações de Strings
