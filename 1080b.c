364478e5b5a95114f21f15b602b4c93ecb43571e
VictorCruz07 <victor.cruz22@unifesp.br>
2026-06-20 11:27:30 -0300

    Update and rename 1080b.C to 1080b.c

--- 1080b.c
+++ 1080b.c
@@ -2,8 +2,7 @@
 Disciplina  : Lógica de Programação, turma IB, 2026S1
 Linguagem   : C
 Nome        : Victor Cruz de Farias
-Plataforma  : https://judge.beecrowd.com/pt/search
-Exercício   : 1080
+Exercício   : https://judge.beecrowd.com/pt/problems/view/1080
 Data        : 21/04/2026
 Objetivo    : Mostrar o Maior numero e a posição dele com o uso de array
 Aprendizado : usar o array
