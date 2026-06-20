a7caba5221efdb6b5ca53ec7dd128dfd4eb7781a
VictorCruz07 <victor.cruz22@unifesp.br>
2026-06-20 11:29:02 -0300

    Update and rename 1253.C to 1253.c

--- 1253.c
+++ 1253.c
@@ -2,8 +2,7 @@
 Disciplina  : Lógica de Programação, turma IB, 2026S1
 Linguagem   : C
 Nome        : Victor Cruz de Farias
-Plataforma  : https://judge.beecrowd.com/pt/search
-Exercício   : 1253
+Exercício   : https://judge.beecrowd.com/pt/problems/view/1253
 Data        : 28/04/2026
 Objetivo    : usar strings
 Aprendizado : comandos de strings
