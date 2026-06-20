157c0c7b7602f5f54a2d2b4a7dd7cb2c7db15660
VictorCruz07 <victor.cruz22@unifesp.br>
2026-06-20 11:28:06 -0300

    Update and rename 1168.C to 1168.c

--- 1168.c
+++ 1168.c
@@ -2,8 +2,7 @@
 Disciplina  : Lógica de Programação, turma IB, 2026S1
 Linguagem   : C
 Nome        : Victor Cruz de Farias
-Plataforma  : https://judge.beecrowd.com/pt/search
-Exercício   : 1168
+Exercício   : https://judge.beecrowd.com/pt/problems/view/1168
 Data        : 22/05/2026
 Objetivo    : Contagem de Leds
 Aprendizado : Usar recursos de strings e funções 
