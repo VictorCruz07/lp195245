a9597b90bc2cc9d36f0ddc7d696ec2f03de1a869
VictorCruz07 <victor.cruz22@unifesp.br>
2026-06-20 11:24:28 -0300

    Update exercise link in comments

--- 1000.c
+++ 1000.c
@@ -2,8 +2,7 @@
 Disciplina  : Lógica de Programação, turma IB, 2026S1
 Linguagem   : C
 Nome        : Victor Cruz de Farias
-Plataforma  : https://judge.beecrowd.com/pt/search
-Exercício   : 1000
+Exercício   : https://judge.beecrowd.com/pt/problems/view/1000
 Data        : 10/04/2026
 Objetivo    : Escrever Hello World
 Aprendizado : inicio da linguagem C
